8c7eb92b7114ded746c4824535d2f37f81cdeca2
dmitry-catch <samohin1dmitry@gmail.com>
2023-06-23 17:46:35 +0300

    context menu fix

--- src/ui/layout/Dropdown.vue
+++ src/ui/layout/Dropdown.vue
@@ -1,6 +1,9 @@
 <style>
 .Dropdown {
 	position: fixed;
+	background: var(--design-background-color-primary);
+	border: 1px solid var(--design-border-color-primary);
+	border-radius: var(--design-border-radius-control);
 	top: v-bind(top);
 	left: v-bind(left);
 }
@@ -13,21 +16,33 @@
 </template>
 
 <script setup lang="ts">
-import { onMounted, ref, toRefs, watchEffect } from 'vue'
+import { ref, toRefs, watchEffect, computed } from 'vue'
 
 const props = defineProps<{ open: boolean }>()
 const { open } = toRefs(props)
 
 const root = ref<HTMLElement>()
-const top = ref('0')
 const left = ref('0')
+const upper = ref(0)
+const menuHeight = ref(0)
+
+const top = computed(() => {
+	const screenHeight = window.innerHeight
+	const topBound = upper.value
+	const bottomBound = topBound + menuHeight.value
+	if (bottomBound > screenHeight) {
+		const newTop = screenHeight - menuHeight.value
+		return `${newTop}px`
+	}
+	return `${topBound}px`
+})
 
 watchEffect(() => {
 	if (open.value) {
 		const rect = root.value?.parentElement?.getBoundingClientRect()
-		if (rect == null) return
-		top.value = `${rect?.bottom + 5}px`
-		left.value = `${rect?.left + 0}px`
+		left.value = rect?.left + 0 + 'px'
+		upper.value = rect?.bottom
+		menuHeight.value = root.value?.scrollHeight + 5
 	}
 })
 </script>
