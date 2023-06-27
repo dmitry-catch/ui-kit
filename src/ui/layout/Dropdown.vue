850b910e32c6be30e872d97935e9440bb90f5c03
dmitry-catch <samohin1dmitry@gmail.com>
2023-06-27 15:23:40 +0300

    fixes

--- src/ui/layout/Dropdown.vue
+++ src/ui/layout/Dropdown.vue
@@ -13,32 +13,20 @@
 </template>
 
 <script setup lang="ts">
-import { ref, toRefs, watchEffect, computed } from 'vue'
+import { onMounted, ref, toRefs, watchEffect } from 'vue'
 
 const props = defineProps<{ open: boolean }>()
 const { open } = toRefs(props)
 
 const root = ref<HTMLElement>()
+const top = ref('0')
 const left = ref('0')
-const baseTop = ref(0)
-const menuHeight = ref(0)
-
-const top = computed(() => {
-	const screenHeight = window.innerHeight
-	const topBound = baseTop.value
-	const bottomBound = topBound + menuHeight.value
-	if (bottomBound > screenHeight) {
-		const newTop = screenHeight - menuHeight.value
-		return `${newTop}px`
-	}
-	return `${topBound}px`
-})
 
 watchEffect(() => {
 	if (open.value) {
 		const rect = root.value?.parentElement?.getBoundingClientRect()
 		if (rect == null) return
-		baseTop.value = `${rect?.bottom + 5}px`
+		top.value = `${rect?.bottom + 5}px`
 		left.value = `${rect?.left + 0}px`
 	}
 })
