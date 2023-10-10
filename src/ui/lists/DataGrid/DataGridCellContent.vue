7c8e534fc8fc701ad4e64178003a65e4db1a36b8
dmitry-catch <samohin1dmitry@gmail.com>
2023-10-10 13:18:07 +0300

    fix date render

--- src/ui/lists/DataGrid/DataGridCellContent.vue
+++ src/ui/lists/DataGrid/DataGridCellContent.vue
@@ -1,18 +1,13 @@
 <style></style>
 
 <template>
-	{{ template(item[column.field]) }}
+	{{ item[column.field] }}
 </template>
 
 <script setup lang="ts">
 import { toRefs } from 'vue'
-import { DataGridColumn, isEnumColumn } from './DataGridColumn'
+import { DataGridColumn } from './DataGridColumn'
 
 const props = defineProps<{ column: DataGridColumn; item: any }>()
 const { item, column } = toRefs(props)
-
-const template = (value: any) => {
-	if (isEnumColumn(column.value)) return column.value.template(value)
-	return value
-}
 </script>
