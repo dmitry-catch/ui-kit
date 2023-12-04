4d36c685abda9bbf83d148e6f1d869c6e9c6f6a6
dmitry-catch <samohin1dmitry@gmail.com>
2023-12-04 11:06:37 +0300

    datepicker sfc change

--- src/components/data-display/DataGrid/utils/index.ts
+++ src/components/data-display/DataGrid/utils/index.ts
@@ -3,19 +3,15 @@ import { BaseGridColumn, CustomizedGridColumn, DataGridColumn, EnumGridColumn, T
 export const isColumn = (column: DataGridColumn): column is BaseGridColumn => {
 	return isTypedColumn(column) && column.type === 'enum'
 }
-
 export const isTypedColumn = (column: DataGridColumn): column is TypedGridColumn => {
 	return 'type' in column
 }
-
 export const isCustomizedColumn = (column: DataGridColumn): column is CustomizedGridColumn => {
 	return 'cellComponent' in column
 }
-
 export const isEnumColumn = (column: DataGridColumn): column is EnumGridColumn => {
 	return isTypedColumn(column) && column.type === 'enum'
 }
-
 export const isDateColumn = (column: DataGridColumn): column is TypedGridColumn => {
 	return isTypedColumn(column) && column.type === 'date'
 }
