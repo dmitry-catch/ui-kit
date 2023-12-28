5a58b3a17d6ca5bd2aa9dd885514526cee9b2fe0
dmitry-catch <samohin1dmitry@gmail.com>
2023-12-28 19:51:56 +0300

    reset button

--- src/components/general/Spinner/Spinner.spec.ts
+++ src/components/general/Spinner/Spinner.spec.ts
@@ -22,46 +22,4 @@ describe(`Component ${Component.name}`, () => {
 		expect(spinner).toBeTruthy()
 		expect(spinner?.getAttribute('variant')).toBe('dark')
 	})
-
-	it('it should be centered when center prop is true', () => {
-		const { container } = render(Component, { props: { center: true } })
-		const spinner = container.querySelector('.Spinner')
-		expect(spinner).toBeTruthy()
-		expect(spinner?.classList.contains('center')).toBe(true)
-	})
-
-	it('it should be inverted when inverse prop is true', () => {
-		const { container } = render(Component, { props: { inverse: true } })
-		const spinner = container.querySelector('.Spinner')
-		expect(spinner).toBeTruthy()
-		expect(spinner?.classList.contains('inverse')).toBe(true)
-	})
-
-	it('it should show the backdrop when backdrop prop is true', () => {
-		const { container } = render(Component, { props: { backdrop: true } })
-		const spinner = container.querySelector('.Spinner')
-		expect(spinner).toBeTruthy()
-		expect(spinner?.classList.contains('backdrop')).toBe(true)
-	})
-
-	it('it should change size when size prop is set', () => {
-		const { container } = render(Component, { props: { size: 'lg' } })
-		const spinner = container.querySelector('.Spinner')
-		expect(spinner).toBeTruthy()
-		expect(spinner?.classList.contains('size-lg')).toBe(true)
-	})
-
-	it('it should change speed when speed prop is set', () => {
-		const { container } = render(Component, { props: { speed: 'fast' } })
-		const spinner = container.querySelector('.Spinner')
-		expect(spinner).toBeTruthy()
-		expect(spinner?.classList.contains('speed-fast')).toBe(true)
-	})
-
-	it('it should be vertical when vertical prop is true', () => {
-		const { container } = render(Component, { props: { vertical: true } })
-		const spinner = container.querySelector('.Spinner')
-		expect(spinner).toBeTruthy()
-		expect(spinner?.classList.contains('vertical')).toBe(true)
-	})
 })
--- src/components/general/Spinner/Spinner.stories.ts
+++ src/components/general/Spinner/Spinner.stories.ts
@@ -3,156 +3,24 @@ import Spinner from './Spinner.vue'
 
 export default {
 	component: Spinner,
-	args: {
-		center: false,
-		inverse: false,
-		backdrop: false,
-		variant: 'light',
-		size: 'md',
-		speed: 'normal',
-		vertical: false
-	},
-	argTypes: {
-		size: {
-			control: 'select',
-			options: ['sm', 'md', 'xs', 'lg', 'xl']
-		},
-		speed: {
-			control: 'select',
-			options: ['slow', 'normal', 'fast']
-		},
-		variant: {
-			control: 'select',
-			options: ['light', 'dark']
-		}
-	}
+	render: (args) => ({
+		args: {},
+		components: { Spinner },
+		setup: () => ({ args }),
+		template: `
+      <div>
+        <Spinner :variant='args.variant'/> 
+      </div>
+    `
+	})
 } satisfies Meta<typeof Spinner>
 
 type Story = StoryObj<typeof Spinner>
 
 export const Default: Story = {}
-export const Inverse: Story = {
-	args: {
-		inverse: true
-	}
-}
 
 export const Dark: Story = {
 	args: {
 		variant: 'dark'
 	}
-}
-export const DarkInverse: Story = {
-	args: {
-		variant: 'dark',
-		inverse: true
-	}
-}
-export const Center: Story = {
-	args: {
-		center: true
-	},
-	render: (args) => ({
-		args: {},
-		components: { Spinner },
-		setup: () => ({ args }),
-		template: `
-      <div style="background: var(--design-background-color-primary); width: 450px; height: 200px; position: relative">
-        <Spinner v-bind='args'>
-			<template #default>
-				Loading...
-			</template>
-		</Spinner> 
-      </div>
-    `
-	})
-}
-export const Backdrop: Story = {
-	args: {
-		center: true,
-		backdrop: true
-	},
-	render: (args) => ({
-		args: {},
-		components: { Spinner },
-		setup: () => ({ args }),
-		template: `
-      <div style="background: var(--design-background-color-primary); width: 450px; height: 200px; position: relative">
-        <Spinner v-bind='args'>
-			<template #default>
-				Loading...
-			</template>
-		</Spinner> 
-      </div>
-    `
-	})
-}
-export const ExtraSmallSize: Story = {
-	args: {
-		size: 'xs'
-	}
-}
-export const SmallSize: Story = {
-	args: {
-		size: 'sm'
-	}
-}
-export const LargeSize: Story = {
-	args: {
-		size: 'lg'
-	}
-}
-export const ExtraLargeSize: Story = {
-	args: {
-		size: 'xl'
-	}
-}
-export const SlowSpeed: Story = {
-	args: {
-		speed: 'slow'
-	}
-}
-export const FastSpeed: Story = {
-	args: {
-		speed: 'fast'
-	}
-}
-export const CustomDescription: Story = {
-	args: {
-		size: 'lg'
-	},
-	render: (args) => ({
-		args: {},
-		components: { Spinner },
-		setup: () => ({ args }),
-		template: `
-      <div>
-        <Spinner v-bind='args'>
-			<template #default>
-				Loading...
-			</template>
-		</Spinner> 
-      </div>
-    `
-	})
-}
-export const CustomDescriptionVertically: Story = {
-	args: {
-		size: 'lg',
-		vertical: true
-	},
-	render: (args) => ({
-		args: {},
-		components: { Spinner },
-		setup: () => ({ args }),
-		template: `
-      <div>
-        <Spinner v-bind='args'>
-			<template #default>
-				Loading...
-			</template>
-		</Spinner> 
-      </div>
-    `
-	})
 }
