83e4361d1ec4250ccbdee78887af30da5213f105
dmitry-catch <samohin1dmitry@gmail.com>
2023-12-29 16:39:46 +0300

    drawer props

--- src/components/general/Spinner/Spinner.stories.ts
+++ src/components/general/Spinner/Spinner.stories.ts
@@ -3,156 +3,24 @@ import Spinner from './Spinner.vue'
 
 export default {
 	component: Spinner,
-	args: {
-		center: false,
-		inverse: false,
-		backdrop: false,
-		variant: 'light',
-		size: 'medium',
-		speed: 'normal',
-		vertical: false
-	},
-	argTypes: {
-		size: {
-			control: 'select',
-			options: ['extra-small', 'small', 'medium', 'large', 'extra-large']
-		},
-		speed: {
-			control: 'select',
-			options: ['slow', 'normal', 'fast']
-		},
-		variant: {
-			control: 'select',
-			options: ['light', 'dark']
-		}
-	}
+	render: (args) => ({
+		args: {},
+		components: { Spinner },
+		setup: () => ({ args }),
+		template: `
+      <div>
+        <Spinner :variant='args.variant'/> 
+      </div>
+    `
+	})
 } satisfies Meta<typeof Spinner>
 
 type Story = StoryObj<typeof Spinner>
 
 export const Default: Story = {}
-export const Inverse: Story = {
-	args: {
-		inverse: true
-	}
-}
 
 export const Dark: Story = {
 	args: {
 		variant: 'dark'
 	}
-}
-export const DarkInverse: Story = {
-	args: {
-		variant: 'dark',
-		inverse: true
-	}
-}
-export const Center: Story = {
-	args: {
-		center: true
-	},
-	render: (args) => ({
-		args: {},
-		components: { Spinner },
-		setup: () => ({ args }),
-		template: `
-      <div style="background: var(--design-background-color-primary); width: 450px; height: 200px; position: relative">
-        <Spinner v-bind='args'>
-			<template #default>
-				Loading...
-			</template>
-		</Spinner> 
-      </div>
-    `
-	})
-}
-export const Backdrop: Story = {
-	args: {
-		center: true,
-		backdrop: true
-	},
-	render: (args) => ({
-		args: {},
-		components: { Spinner },
-		setup: () => ({ args }),
-		template: `
-      <div style="background: var(--design-background-color-primary); width: 450px; height: 200px; position: relative">
-        <Spinner v-bind='args'>
-			<template #default>
-				Loading...
-			</template>
-		</Spinner> 
-      </div>
-    `
-	})
-}
-export const ExtraSmallSize: Story = {
-	args: {
-		size: 'extra-small'
-	}
-}
-export const SmallSize: Story = {
-	args: {
-		size: 'small'
-	}
-}
-export const LargeSize: Story = {
-	args: {
-		size: 'large'
-	}
-}
-export const ExtraLargeSize: Story = {
-	args: {
-		size: 'extra-large'
-	}
-}
-export const SlowSpeed: Story = {
-	args: {
-		speed: 'slow'
-	}
-}
-export const FastSpeed: Story = {
-	args: {
-		speed: 'fast'
-	}
-}
-export const CustomDescription: Story = {
-	args: {
-		size: 'large'
-	},
-	render: (args) => ({
-		args: {},
-		components: { Spinner },
-		setup: () => ({ args }),
-		template: `
-      <div>
-        <Spinner v-bind='args'>
-			<template #default>
-				Loading...
-			</template>
-		</Spinner> 
-      </div>
-    `
-	})
-}
-export const CustomDescriptionVertically: Story = {
-	args: {
-		size: 'large',
-		vertical: true
-	},
-	render: (args) => ({
-		args: {},
-		components: { Spinner },
-		setup: () => ({ args }),
-		template: `
-      <div>
-        <Spinner v-bind='args'>
-			<template #default>
-				Loading...
-			</template>
-		</Spinner> 
-      </div>
-    `
-	})
 }
--- src/components/general/Spinner/Spinner.vue
+++ src/components/general/Spinner/Spinner.vue
@@ -2,53 +2,22 @@
 import { toRefs } from 'vue'
 
 interface SpinnerProps {
-	/** Центрирование в контейнере */
-	center?: boolean
-	/** Инвертирование цветов */
-	inverse?: boolean
-	/** Скрытие отображения заднего фонa */
-	backdrop?: boolean
-	/** 2 варианта окраски спиннера */
-	variant?: 'light' | 'dark'
-	size?: 'large' | 'medium' | 'small' | 'extra-small' | 'extra-large'
-	speed?: 'fast' | 'normal' | 'slow'
-	vertical?: boolean
+	variant?: string
 }
 
 const props = withDefaults(defineProps<SpinnerProps>(), {
-	variant: 'light',
-	size: 'medium',
-	speed: 'normal'
+	variant: 'light'
 })
 
-const { variant, size, speed, inverse, center, backdrop } = toRefs(props)
+const { variant } = toRefs(props)
 </script>
 
 <template>
-	<div class="Spinner" :class="{ vertical, center, backdrop }">
-		<div class="Spinner__wheel" :class="{ inverse }" :variant="variant" :size="size" :speed="speed"></div>
-		<div class="Spinner__hint" :class="{ inverse }">
-			<slot></slot>
-		</div>
-	</div>
+	<div class="Spinner" :variant="variant"></div>
 </template>
 
-<style scoped>
+<style>
 .Spinner {
-	display: flex;
-	align-items: center;
-	justify-content: center;
-	gap: var(--design-gap-unit);
-}
-
-.Spinner.backdrop::backdrop {
-	background: var(--design-background-color-modal-shadow);
-}
-
-.Spinner.vertical {
-	flex-direction: column;
-}
-.Spinner__wheel {
 	width: fit-content;
 	min-height: var(--design-current-font-size);
 	min-width: var(--design-current-font-size);
@@ -57,62 +26,9 @@ const { variant, size, speed, inverse, center, backdrop } = toRefs(props)
 	border-top-color: var(--design-background-color-primary);
 	animation: spin 1.2s ease-in-out infinite;
 }
-.Spinner__wheel.inverse {
-	border: 2px solid var(--design-background-color-primary);
-	border-top-color: var(--design-border-color-accent-primary);
-}
-.Spinner__wheel[variant='dark'].inverse {
-	border: 3px solid var(--design-border-color-accent-primary);
-	border-top-color: var(--design-text-color-on-accent-tertiary);
-}
-.Spinner__hint.inverse {
-	color: var(--design-text-color-on-accent-primary);
-}
-.Spinner__wheel[variant='dark'] {
+.Spinner[variant='dark'] {
 	border: 3px solid var(--design-text-color-on-accent-tertiary);
 	border-top-color: var(--design-border-color-accent-primary);
-}
-.Spinner__wheel[size='small'] {
-	height: calc(var(--design-font-size-footnote) * 0.75);
-	width: calc(var(--design-font-size-footnote) * 0.75);
-	min-height: 0;
-	min-width: 0;
-}
-.Spinner__wheel[variant='dark'][size*='large'] {
-	background-image: url(./src/spinner-large.svg);
-	background-size: contain;
-	background-repeat: no-repeat;
-	border: none;
-}
-.Spinner__wheel[size='large'] {
-	height: calc(var(--design-font-size-large) * 2);
-	width: calc(var(--design-font-size-large) * 2);
-}
-.Spinner__wheel[size='extra-small'] {
-	min-height: 0;
-	min-width: 0;
-	height: calc(var(--design-font-size-hint) * 0.2);
-	width: calc(var(--design-font-size-hint) * 0.2);
-}
-.Spinner__wheel[size='extra-large'] {
-	height: 68px;
-	width: 68px;
-}
-.Spinner__wheel[speed='slow'] {
-	animation-duration: 1.75s;
-}
-.Spinner__wheel[speed='fast'] {
-	animation-duration: 0.75s;
-}
-.Spinner.center {
-	position: absolute;
-	top: 0;
-	left: 0;
-	right: 0;
-	bottom: 0;
-	margin: auto;
-	width: 100%;
-	height: 100%;
 }
 
 @keyframes spin {
