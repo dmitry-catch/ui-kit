d3462f224dee3488ff60459bd215b73ca4d91112
dmitry-catch <samohin1dmitry@gmail.com>
2024-01-24 01:22:59 +0300

    some fix

--- src/components/data-display/Dropdown/Dropdown.stories.ts
+++ src/components/data-display/Dropdown/Dropdown.stories.ts
@@ -1,30 +1,7 @@
 import { Meta, StoryObj } from '@storybook/vue3'
 import Dropdown from './Dropdown.vue'
+import DropdownItem from './DropdownItem/DropdownItem.vue'
 import Icon from '../../general/Icon/Icon.vue'
-
-const dropdownItems = [
-	{ label: 'Item 1', value: 'value1', action: () => console.log('selected Item 1.') },
-	{ label: 'Item 2', value: 'value2', action: () => console.log('selected Item 2.') },
-	{ label: 'Item 3', value: 'value3', action: () => console.log('selected Item 3.') },
-	{ label: 'Item 4', value: 'value4', action: () => console.log('selected Item 4.') }
-]
-
-const dropdownGroupItems = [
-	{
-		name: 'Group1:',
-		items: [
-			{ label: 'SubItem 1.1', value: 'value1.1', action: () => console.log('selected option 1.1.') },
-			{ label: 'SubItem 1.2', value: 'value1.2', action: () => console.log('selected option 1.2.') }
-		]
-	},
-	{
-		name: 'Group2:',
-		items: [
-			{ label: 'SubItem 2.2', value: 'value1.1', action: () => console.log('selected option 1.1.') },
-			{ label: 'SubItem 2.2', value: 'value1.2', action: () => console.log('selected option 1.2.') }
-		]
-	}
-]
 
 export default {
 	component: Dropdown,
@@ -34,7 +11,29 @@ export default {
 		caret: true,
 		icon: 'internet',
 		loading: false,
-		items: dropdownItems
+		items: [
+			{
+				label: 'Item 1',
+				value: 'value1',
+				extraAttrs: { disabled: true },
+				wrapperClass: 'Dropdown__wrapperClass',
+				action: () => console.log('selected Item 1.')
+			},
+			{ label: 'Item 2', value: 'value2', action: () => console.log('selected Item 2.') },
+			{
+				name: 'Группа 1',
+				
+				items: [
+					{
+						label: 'SubItem 1',
+						value: 'value1.1',
+						
+						action: () => console.log('selected option 1.1.')
+					},
+					{ label: 'SubItem 2', value: 'value1.2', action: () => console.log('selected option 1.2.'), extraAttrs: { disabled: true } }
+				]
+			}
+		]
 	},
 	argTypes: {
 		size: {
@@ -49,90 +48,14 @@ export default {
 			control: 'select',
 			options: [true, 'keyboard', 'outside', 'item']
 		}
-	}
-} satisfies Meta<typeof Dropdown>
-
-type Story = StoryObj<typeof Dropdown>
-
-export const Default: Story = {}
-
-export const CustomItems: Story = {
-	render: (args) => ({
-		components: { Dropdown },
-		setup() {
-			return { args }
-		},
-		template: `
-		<Dropdown v-bind="args">
-		  <template #item="{ item }">
-			<i>{{ item.label }}</i>
-		  </template>
-		</Dropdown>
-	  `
-	})
-}
-
-export const Groups: Story = {
-	args: {
-		items: dropdownGroupItems
-	}
-}
-
-export const CustomGroups: Story = {
-	args: {
-		items: dropdownGroupItems
 	},
-	render: (args) => ({
-		components: { Dropdown },
-		setup() {
-			return { args }
-		},
-		template: `
-	  <Dropdown v-bind="args">
-		<template #groupLabel="{ group }">
-		  <i>{{ group.name }}</i>
-		</template>
-	  </Dropdown>
-	  `
-	})
-}
-
-export const CustomToggle: Story = {
-	render: (args) => ({
-		components: { Dropdown, Icon },
-		setup() {
-			return { args }
-		},
+    render: (args) => ({
+		components: { Dropdown, DropdownItem, Icon },
+		setup: () => ({ args }),
 		template: `
 		<Dropdown v-bind="args">
-			<template #toggle>
-				<Icon name='edit' />
-			</template>
-		</Dropdown>
-	  `
-	})
-}
-
-export const TextContent: Story = {
-	args: {
-		default: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit.'
-	}
-}
-
-export const MiscellaneousContent: Story = {
-	args: {
-		items: [...dropdownItems, ...dropdownGroupItems]
-	},
-	render: (args) => ({
-		components: { Dropdown },
-		setup() {
-			return { args }
-		},
-		template: `
-		<Dropdown v-bind="args">
-			<template #header>
-				<b>Dropdown header</b>
-			</template>
+			<template #toggle> Toggle </template>
+			<template #header> Dropdown header </template>
 			<template #item="{ item }">
 				<div>{{ item.label }}</div>
 			</template>
@@ -143,6 +66,11 @@ export const MiscellaneousContent: Story = {
 				<i>Footer</i>
 			</template>
 		</Dropdown>
-	  `
+		`
 	})
-}
+
+} satisfies Meta<typeof Dropdown>
+
+type Story = StoryObj<typeof Dropdown>
+
+export const Default: Story = {}
