a5ae953845e1e5c756da1b590ff426e2e84d6465
dmitry-catch <samohin1dmitry@gmail.com>
2024-01-30 20:01:14 +0300

    block prop and button styles

--- src/components/general/Button/Button.vue
+++ src/components/general/Button/Button.vue
@@ -12,6 +12,7 @@ interface BtnProps {
 	disabled?: boolean
 	size?: 'medium' | 'small' | 'extra-small'
 	loading?: boolean
+	block?: boolean
 }
 
 const props = withDefaults(defineProps<BtnProps>(), {
@@ -21,7 +22,7 @@ const props = withDefaults(defineProps<BtnProps>(), {
 
 const root = ref()
 
-const { dropdown, disabled, loading } = toRefs(props)
+const { dropdown, disabled, loading, block } = toRefs(props)
 
 const hasDropdown = computed(() => !!dropdown?.value?.length)
 const dropdownOpened = ref(false)
@@ -33,7 +34,7 @@ const clickOutside = (event: Event) => {
 </script>
 
 <template>
-	<div ref="root" class="Btn" :class="{ disabled: disabled }">
+	<div ref="root" class="Btn" :class="{ disabled, block }">
 		<button class="Btn__actual accent" :class="[props.size]" :disabled="disabled" @click="toggleDropdown">
 			<Spinner v-if="loading" />
 			<slot name="before"></slot>
@@ -63,6 +64,10 @@ const clickOutside = (event: Event) => {
 	--button-background-color-primary: transparent;
 }
 
+.Btn.block {
+	width: 100%;
+}
+
 .Btn__actual {
 	border: 1px solid var(--button-border-color-primary);
 	border-radius: var(--design-border-radius-control);
@@ -76,6 +81,7 @@ const clickOutside = (event: Event) => {
 	box-sizing: border-box;
 	height: min-content;
 	white-space: nowrap;
+	width: 100%;
 }
 
 .Btn__actual:focus-visible {
@@ -173,7 +179,6 @@ const clickOutside = (event: Event) => {
 	cursor: pointer;
 	border: none;
 	padding: 0;
-	justify-content: start;
 }
 
 .Btn.functional.disabled .Btn__actual {
@@ -197,12 +202,10 @@ const clickOutside = (event: Event) => {
 .Btn__actual.small {
 	padding: calc(var(--design-gap-unit) / 2) calc(var(--design-gap-unit) * 2);
 	font-size: var(--design-font-size-small);
-	--icon-size: 20px;
 }
 
 .Btn__actual.extra-small {
 	padding: calc(var(--design-gap-unit) / 2) calc(var(--design-gap-unit) * 2);
 	font-size: var(--design-font-size-footnote);
-	--icon-size: 18px;
 }
 </style>
