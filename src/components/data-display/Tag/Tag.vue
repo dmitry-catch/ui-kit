5b3db1349f156367c4cc8945b543cb7ac14d71f3
dmitry-catch <samohin1dmitry@gmail.com>
2024-03-23 01:44:50 +0000

    Merge branch 'main'

--- src/components/data-display/Tag/Tag.vue
+++ src/components/data-display/Tag/Tag.vue
@@ -58,6 +58,7 @@ const { color, closable, squared } = toRefs(props)
 .Tag--squared {
 	border-radius: 4px;
 }
+
 .Tag[size='extra-small'] {
 	padding: calc(0.1 * var(--design-gap-unit)) calc(1.5 * var(--design-gap-unit));
 }
