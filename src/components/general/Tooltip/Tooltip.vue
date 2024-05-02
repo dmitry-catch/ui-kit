20bcfb8a72be51d9caa4c509ac6b8cfcfb64175a
dmitry-catch <samohin1dmitry@gmail.com>
2024-05-02 16:00:16 +0300

    Add toasts

--- src/components/general/Tooltip/Tooltip.vue
+++ src/components/general/Tooltip/Tooltip.vue
@@ -26,140 +26,138 @@ const { placement, arrowed, delay, disabled, offset } = toRefs(props)
 
 const open = defineModel<boolean>({ type: Boolean })
 
-const container = ref<HTMLElement>()
+const tippedContent = ref<HTMLElement>()
 const content = ref<HTMLElement>()
 const contentWidth = ref<number>()
 const contentHeight = ref<number>()
 
+const tippedContentRect = computed(() => tippedContent.value?.getBoundingClientRect() ?? {})
 const topOffset = computed(() => {
 	const tipHeight = contentHeight.value ?? Number(content.value?.getBoundingClientRect().height)
-	const sizeDifference = Number(container.value?.offsetHeight) - tipHeight
+	const sizeDifference = Math.abs(Number(tippedContentRect.value?.height) - tipHeight)
 	switch (placement.value) {
 		case 'top':
-			return -tipHeight - Number(container.value?.offsetHeight) - offset.value - 4 + 'px'
+			return Math.abs(Number(tippedContentRect.value?.top)) - Math.abs(tipHeight) - 4 - offset.value + 'px'
 		case 'bottom':
-			return 4 + offset.value + 'px'
+			return Math.abs(Number(tippedContentRect.value?.bottom)) + 4 + offset.value + 'px'
 		default:
-			return -tipHeight - sizeDifference / 2 + 'px'
+			return tippedContentRect.value?.top + sizeDifference / 2 + 'px'
 	}
 })
 
 const leftOffset = computed(() => {
-	const elementWidth = contentWidth.value ?? Number(content.value?.offsetWidth)
-	const sizeDifference = Number(Number(container.value?.offsetWidth) - elementWidth) / 2
+	const elementWidth = contentWidth.value ?? Number(content.value?.scrollWidth)
+	const initialLeft = elementWidth / 2
+	const tippedX = Math.abs(Number(tippedContentRect.value?.x))
 	switch (placement.value) {
 		case 'left':
-			return -Math.abs(Number(content.value?.offsetWidth)) - 4 + 'px'
+			return tippedX - Math.abs(elementWidth) - offset.value - 4 + 'px'
 		case 'right':
-			return Math.abs(Number(container.value?.offsetWidth)) + 4 + 'px'
+			return tippedX + Math.abs(Number(tippedContentRect.value?.width)) + offset.value + 4 + 'px'
 		default:
-			return sizeDifference + 'px'
+			return tippedX - initialLeft + Number(tippedContentRect.value?.width) / 2 + 'px'
 	}
 })
-
 const onHover = () => {
-	open.value = true
 	contentWidth.value = Number(content.value?.offsetWidth)
-	contentHeight.value = Number(content.value?.offsetHeight)
+	contentHeight.value = Number(content.value?.clientHeight)
 }
-const onLeave = () => (open.value = false)
 
 useModalContext(content)
 </script>
 <template>
-	<div ref="container" class="TooltipContainer">
-		<div class="tippedContent" data-testid="trigger" @mouseover="onHover" @mouseout="onLeave">
-			<slot name="default"> </slot>
-		</div>
-		<div :style="{ position: 'fixed' }">
-			<div
-				v-if="!disabled"
-				ref="content"
-				class="Tooltip"
-				data-testid="content"
-				:style="{ transitionDuration: delay + 'ms' }"
-				:placement
-				:open
-			>
-				<div><slot name="tooltip"></slot></div>
-				<svg
-					v-if="arrowed"
-					class="arrow"
-					xmlns="http://www.w3.org/2000/svg"
-					width="132"
-					height="4"
-					viewBox="0 0 132 4"
-					fill="none"
-				>
-					<path d="M72 0L60 0L66 4L72 0Z" />
-				</svg>
-			</div>
-		</div>
+	<div ref="tippedContent" class="tippedContent" data-testid="trigger" @mouseover="onHover">
+		<slot name="default"> </slot>
+	</div>
+	<div
+		v-if="!disabled"
+		ref="content"
+		class="Tooltip"
+		data-testid="content"
+		:class="$attrs.class"
+		:style="{ transitionDuration: delay + 'ms' }"
+		:placement
+		:open
+	>
+		<div><slot name="tooltip"></slot></div>
+		<svg
+			v-if="arrowed"
+			class="arrow"
+			xmlns="http://www.w3.org/2000/svg"
+			width="132"
+			height="4"
+			viewBox="0 0 132 4"
+			fill="none"
+		>
+			<path d="M72 0L60 0L66 4L72 0Z" />
+		</svg>
 	</div>
 </template>
 <style scoped>
-.TooltipContainer {
-	overflow: visible;
-	position: relative;
-	.Tooltip {
-		z-index: 1000;
-		transition: ease-in-out;
-		box-shadow: 0px 12px 24px 0px rgba(33, 44, 58, 0.16);
-		border-radius: var(--design-border-radius-control);
-		background-color: var(--design-background-color-primary);
-		padding: calc(0.5 * var(--design-gap-unit)) calc(2 * var(--design-gap-unit));
-		text-align: center;
-		position: absolute;
-		left: v-bind(leftOffset);
-		top: v-bind(topOffset);
-		width: max-content;
-		.arrow {
-			z-index: 1000;
-			max-width: 100%;
-			position: absolute;
-			left: 0;
-			right: 0;
-			margin-left: auto;
-			margin-right: auto;
-			bottom: -3px;
-		}
-		.arrow > path {
-			fill: var(--design-background-color-primary);
-		}
-	}
-	.Tooltip[placement='bottom'] {
-		box-shadow: 0px -12px 24px 0px rgba(33, 44, 58, 0.16);
-		.arrow {
-			transform: rotateY('angle');
-			bottom: unset;
-			top: -3px;
-			transform: rotate(180deg);
-		}
-	}
+.Tooltip {
+	transition: ease-in-out;
+	box-shadow: 0px 12px 24px 0px rgba(33, 44, 58, 0.16);
+	border-radius: var(--design-border-radius-control);
+	background-color: var(--design-background-color-primary);
+	padding: calc(0.5 * var(--design-gap-unit)) calc(2 * var(--design-gap-unit));
+	max-width: 100%;
+	max-height: 100%;
+	width: fit-content;
+	text-align: center;
+	position: fixed;
+	left: v-bind(leftOffset);
+	top: v-bind(topOffset);
 
-	.Tooltip[placement='right'] {
-		box-shadow: -12px 12px 24px 0px rgba(33, 44, 58, 0.16);
-		.arrow {
-			top: 50%;
-			left: calc(-100% - 3px);
-			transform: rotate(90deg);
-		}
+	.arrow {
+		max-width: 100%;
+		position: absolute;
+		left: 0;
+		right: 0;
+		margin-left: auto;
+		margin-right: auto;
+		bottom: -3px;
 	}
-	.Tooltip[placement='left'] {
-		box-shadow: 12px 12px 24px 0px rgba(33, 44, 58, 0.16);
-		.arrow {
-			top: 50%;
-			right: calc(-100% - 3px);
-			transform: rotate(270deg);
-		}
+	.arrow > path {
+		fill: var(--design-background-color-primary);
 	}
-	.Tooltip[open='true'] {
-		visibility: visible;
-		opacity: 1;
+}
+.Tooltip[placement='bottom'] {
+	box-shadow: 0px -12px 24px 0px rgba(33, 44, 58, 0.16);
+	.arrow {
+		transform: rotateY('angle');
+		bottom: unset;
+		top: -3px;
+		transform: rotate(180deg);
 	}
-	.Tooltip[open='false'] {
-		visibility: hidden;
-		opacity: 0;
+}
+
+.Tooltip[placement='right'] {
+	box-shadow: -12px 12px 24px 0px rgba(33, 44, 58, 0.16);
+	.arrow {
+		top: 50%;
+		left: calc(-100% - 3px);
+		transform: rotate(90deg);
 	}
+}
+.Tooltip[placement='left'] {
+	box-shadow: 12px 12px 24px 0px rgba(33, 44, 58, 0.16);
+	.arrow {
+		top: 50%;
+		right: calc(-100% - 3px);
+		transform: rotate(270deg);
+	}
+}
+.Tooltip[open='true'] {
+	visibility: visible;
+	opacity: 1;
+}
+.Tooltip[open='false'] {
+	visibility: hidden;
+	opacity: 0;
+}
+
+.tippedContent:hover + .Tooltip[open='false'] {
+	visibility: visible;
+	opacity: 1;
 }
 </style>
