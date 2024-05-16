82b5ce9e3a54efbb74419ac9ecf8e9771629f39c
dmitry-catch <samohin1dmitry@gmail.com>
2024-05-16 13:25:38 +0300

    format fix

--- src/utils/dateChecker.ts
+++ src/utils/dateChecker.ts
@@ -1,4 +1,4 @@
-const isoDatePattern = new RegExp(/^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}(\.\d{0,})?Z$/)
+const isoDatePattern = new RegExp(/^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}(\.\d{0,})?Z/)
 
 const isISOString = (value: any): boolean =>
 	typeof value === 'string' && !Number.isNaN(Date.parse(value)) && isoDatePattern.test(value)
