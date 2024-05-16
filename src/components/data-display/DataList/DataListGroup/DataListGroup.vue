6ecf8f457a51afa125e7b1d4c9a0a59d685deb74
dmitry-catch <samohin1dmitry@gmail.com>
2024-05-16 13:34:56 +0300

    Merge branch 'main'

--- src/components/data-display/DataList/DataListGroup/DataListGroup.vue
+++ src/components/data-display/DataList/DataListGroup/DataListGroup.vue
@@ -1,45 +1,72 @@
-<script setup lang="ts">
-import { ref, toRefs } from 'vue'
-import { DataListGroupType, DataListItemType } from '../types.js'
+<script setup lang="ts" generic="T">
+import { reactive, ref, toRefs } from 'vue'
+import { DataListGroupType, DataListItemType, DataListLoadContext } from '../types.js'
 import Icon from '../../../general/Icon/Icon.vue'
 
 const props = defineProps<{
-	group: DataListGroupType
+	group: DataListGroupType<T>
 	expandable?: boolean
 	size?: 'extra-small' | 'small' | 'medium'
 }>()
 
+const emit = defineEmits<{
+	(event: 'load', groupContext: DataListLoadContext<T>): void
+}>()
+
 defineSlots<{
 	/** Заголовок группы */
-	groupLabel?: (props: { group: DataListGroupType }) => any
+	groupLabel?: (props: { group: DataListGroupType<T> }) => any
 	/** Элементы внутри группы */
-	groupItems?: (props: { items: DataListItemType[] }) => any
+	groupItems?: (props: { items: DataListItemType<T>[] }) => any
+	/** Загрузить еще */
+	loadMoreGroup?: (props: { loadGroup: () => void; groupContext: DataListLoadContext<T> }) => any
 }>()
 
 const { group, size, expandable } = toRefs(props)
 
 const isCollapsed = ref(group.value.isCollapsed)
 
+const groupContext: DataListLoadContext<T> = reactive({
+	type: 'group',
+	current: group,
+	loading: false,
+	completed: false
+})
+
 const groupClickHandler = () => {
 	if (expandable.value) {
 		isCollapsed.value = !isCollapsed.value
+	}
+}
+const loadGroup = () => {
+	if (!isCollapsed.value) {
+		groupContext.current = group.value
+		emit('load', groupContext)
 	}
 }
 </script>
 
 <template>
 	<div class="DataList__group" v-bind="group.extraAttrs">
-		<div class="DataList__groupLabel" :size="size" @click="groupClickHandler">
+		<div class="DataList__groupLabel" :size="size" @click=";[groupClickHandler(), loadGroup()]">
 			<Icon v-if="expandable" :name="isCollapsed ? 'chevron_down' : 'chevron_up'" />
 			<slot name="groupLabel" :group="group">{{ group.key }}</slot>
 		</div>
 		<div v-if="!isCollapsed" class="DataList__groupItems">
 			<slot name="groupItems" :items="group.data"></slot>
+			<div class="DataList__loadMore">
+				<slot name="loadMoreGroup" :loadGroup="loadGroup" :groupContext="groupContext"> </slot>
+			</div>
 		</div>
 	</div>
 </template>
 
 <style scoped>
+.DataList__loadMore {
+	display: flex;
+	justify-content: center;
+}
+
 .DataList__groupLabel {
 	display: flex;
 	align-items: center;
