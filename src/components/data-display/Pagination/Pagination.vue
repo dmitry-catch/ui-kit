d8813fd127a8c2d6d81522b60da74526a872f6a9
dmitry-catch <samohin1dmitry@gmail.com>
2024-05-27 16:02:24 +0300

    Merge branch 'main'

--- src/components/data-display/Pagination/Pagination.vue
+++ src/components/data-display/Pagination/Pagination.vue
@@ -50,52 +50,58 @@ const toLastPage = () => updatePage(totalPages.value)
 </script>
 
 <template>
-	<div class="Pagination">
-		<div>
+	<div class="DataGridPaginationPanel">
+		<div class="DataGridPaginationPanel__statistics">
 			<span> Записей: {{ currentPage * pageSize }} из {{ totalElements }} </span>
 		</div>
-		<div v-if="totalPages > 1" class="Pagination__buttons">
+		<div v-if="totalPages > 1" class="DataGridPaginationPanel__buttons">
 			<Button
 				v-for="(button, index) of buttons"
 				:key="index"
-				class="Pagination__button functional"
-				:class="{ 'Pagination__button--current': button === currentPage }"
+				class="DataGridPaginationPanel__button functional"
+				:class="{ 'DataGridPaginationPanel__button--current': button === currentPage }"
 				:disabled="totalPages < button"
 				@click="buttonClick(button)"
 			>
-				<span :class="{ 'Pagination__buttonText--current': button === currentPage }" class="accent">
+				<span
+					:class="{ 'DataGridPaginationPanel__buttonText--current': button === currentPage }"
+					class="accent"
+				>
 					{{ button }}
 				</span>
 			</Button>
 			<span v-if="!isLastButtonGroup">...</span>
 			<Button
 				v-if="!isLastTheTotalButton"
-				class="Pagination__button functional"
-				:class="{ 'Pagination__button--current': totalPages === currentPage }"
+				class="DataGridPaginationPanel__button functional"
+				:class="{ 'DataGridPaginationPanel__button--current': totalPages === currentPage }"
 				@click="toLastPage"
 			>
-				<span class="accent" :class="{ 'Pagination__buttonText--current': totalPages === currentPage }">
+				<span
+					class="accent"
+					:class="{ 'DataGridPaginationPanel__buttonText--current': totalPages === currentPage }"
+				>
 					{{ totalPages }}
 				</span>
 			</Button>
-			<Button class="Pagination__button icon functional" @click="toFirstPage">
-				<Icon name="chevron_double_backward" class="Pagination__buttonIcon" />
+			<Button class="DataGridPaginationPanel__button icon functional" @click="toFirstPage">
+				<Icon name="chevron_double_backward" class="DataGridPaginationPanel__buttonIcon" />
 			</Button>
-			<Button class="Pagination__button icon functional" @click="decrementPage">
-				<Icon name="chevron_backward" class="Pagination__buttonIcon" />
+			<Button class="DataGridPaginationPanel__button icon functional" @click="decrementPage">
+				<Icon name="chevron_backward" class="DataGridPaginationPanel__buttonIcon" />
 			</Button>
-			<Button class="Pagination__button icon functional" @click="incrementPage">
-				<Icon name="chevron_forward" class="Pagination__buttonIcon" />
+			<Button class="DataGridPaginationPanel__button icon functional" @click="incrementPage">
+				<Icon name="chevron_forward" class="DataGridPaginationPanel__buttonIcon" />
 			</Button>
-			<Button class="Pagination__button icon functional" @click="toLastPage">
-				<Icon name="chevron_double_forward" class="Pagination__buttonIcon" />
+			<Button class="DataGridPaginationPanel__button icon functional" @click="toLastPage">
+				<Icon name="chevron_double_forward" class="DataGridPaginationPanel__buttonIcon" />
 			</Button>
 		</div>
 	</div>
 </template>
 
 <style scoped>
-.Pagination {
+.DataGridPaginationPanel {
 	position: sticky;
 	left: 0;
 	bottom: 0;
@@ -108,21 +114,21 @@ const toLastPage = () => updatePage(totalPages.value)
 	z-index: 1000;
 }
 
-.Pagination__buttons {
+.DataGridPaginationPanel__buttons {
 	display: flex;
 	align-items: center;
 }
 
-.Pagination__button {
+.DataGridPaginationPanel__button {
 	padding: var(--design-gap-unit) calc(2 * var(--design-gap-unit));
 }
 
-.Pagination__button--current {
+.DataGridPaginationPanel__button--current {
 	border: 1px solid var(--design-border-color-accent-primary);
 	border-radius: var(--design-border-radius-control);
 }
 
-.Pagination__buttonText--current {
+.DataGridPaginationPanel__buttonText--current {
 	color: var(--design-text-color-accent);
 }
 </style>
